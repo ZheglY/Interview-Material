22b13c7b83994846e1f954789ab7682df26be73a
Yaroslav Zh <zhegly.work@gmail.com>
2026-08-19 14:31:02 +0300

    f1-topic

---
+++ go.mod
@@ -0,0 +1,1 @@
+module github.com/ZheglY/Interview-Material
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import interviewdays "github.com/ZheglY/Interview-Material/interview-days"
+
+func main() {
+	interviewdays.D1()
+}
